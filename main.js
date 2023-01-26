af811f1942282f6b931d1c15a2aabc3520c51d7a
Radvir <kovacstihamer500@gmail.com>
2023-01-26 20:53:18 +0100

    started button

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 document.getElementById("body").addEventListener('click', () => {
     window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
+document.getElementById("magic").addEventListener('click', () => {
+    window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+});
