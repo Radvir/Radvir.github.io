cb981e912a4930a1796d2072f4f210ce07ea95a9
Radvir <kovacstihamer500@gmail.com>
2023-01-27 20:38:20 +0100

    translator box

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-document.getElementById("body").addEventListener('click', () => {
-    window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-});
-document.getElementById("magic").addEventListener('click', () => {
+document.getElementById("translator").addEventListener('click', (event) => {
+    event.stopPropagation();
+    window.location.assign("link-to-translator");
+})
+document.getElementById("body").addEventListener("click", () => {
     window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 });
