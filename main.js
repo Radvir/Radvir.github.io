0a58ff8d5f14db15a9b4998dbaacab6b8bc76565
Radvir <vargazenozoltan@gmail.com>
2023-02-02 09:31:48 +0100

    translator link

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.getElementById("translator").addEventListener('click', (event) => {
     event.stopPropagation();
-    window.location.assign("link-to-translator");
+    window.location.assign("https://karesztranslator2.vercel.app/");
 })
 document.getElementById("body").addEventListener("click", () => {
     window.location.assign("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
